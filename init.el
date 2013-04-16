390519161f231ab69e900fa0a70e9063cc92be00
Ilia Mikhailov <ilia.mikhailov@gmail.com>
2013-04-16 21:22:13 +0200

    Moved custom settnings to own file

--- init.el
+++ init.el
@@ -158,7 +158,7 @@
 (setq ring-bell-function (lambda () (message "*beep*")))
 
 ;; smart pairing for all
-(electric-pair-mode t)
+(autopair-mode t)
 
 ;; Gotta see matching parens
 (show-paren-mode t)
@@ -351,10 +351,23 @@
 (setq org-directory "~/Dropbox/Org")
 (setq org-archive-location "~/Dropbox/Org/archive.org::From %s")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
-(setq org-agenda-files (quote ("~/Dropbox/Org")))
+(setq org-todo-keywords
+  '((sequence "TODO(t)" "NEXT(n)" "WIP(w)" "PENDING(p)" "|" "DONE(d)")))
+(setq org-todo-keyword-faces
+   '(("TODO" :foreground "pink" :weight bold)
+     ("NEXT" :foreground "white" :weight bold)
+     ("DONE" :foreground "lime green" :weight bold)
+     ("PENDING" :foreground "yellow" :weight bold)
+     ("WIP" :foreground "orange" :weight bold)))
 
-(require 'org)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(setq org-log-done 'time)
+
+(setq org-capture-templates 
+      '(("t" "todo" entry (file (concat org-directory "/inbox.org")) 
+        "* TODO %?")
+        ("r" "retrospective" plain (file (concat org-directory "/progress.org"))
+         "\n%?\n\nEntered on %u\n\n" :prepend t)))
+
 (define-key global-map "\C-cc" 'org-capture)
 (define-key global-map "\C-cb" 'org-iswitchb)
 (define-key global-map "\C-ca" 'org-agenda)
@@ -416,17 +429,6 @@
       (setq mac-command-modifier 'meta))))
 
 (put 'dired-find-alternate-file 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(custom-safe-themes (quote ("6615e5aefae7d222a0c252c81aac52c4efb2218d35dfbb93c023c4b94d3fa0db" "38c4fb6c8b2625f6307f3dde763d5c61d774d854ecee9c5eb9c5433350bc0bef" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" "fca8ce385e5424064320d2790297f735ecfde494674193b061b9ac371526d059" "02414c4cfbbe9805b89a5ec66d3d3deb4ae1e4795ed2092eab240ca0cb79ea96" "d24e10524bb50385f7631400950ba488fa45560afcadd21e6e03c2f5d0fad194" "f38dd27d6462c0dac285aa95ae28aeb7df7e545f8930688c18960aeaf4e807ed" "f641bdb1b534a06baa5e05ffdb5039fb265fde2764fbfd9a90b0d23b75f3936b" "303488aa27ce49f658a7ba4035e93380421e394ec2799ae8fd952d08808c7235" "159bb8f86836ea30261ece64ac695dc490e871d57107016c09f286146f0dae64" "47d2a01f2cbd853ccd1eddcb0e9e4fdfdabcc97ddad1d1a5218304294889f731" "06f5145c01ec774a0abb49eeffa3980743ce2f997112b537effeb188b7c51caf" "8f6537eb6f9d66b060c736f5f680f5c661e0a6b311b86defa293bc5ba104a030" "3bd9497fb8f39c28ab58a9e957152ba2dc41223c23c5520ef10fc7bd6b222384" "1278386c1d30fc24b4248ba69bc5b49d92981c3476de700a074697d777cb0752" default)))
- '(safe-local-variable-values (quote ((encoding . utf-8)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;; Save custom settings to own file
+(setq custom-file (concat user-emacs-directory "custom-settings.el")
